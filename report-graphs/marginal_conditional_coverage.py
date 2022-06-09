5f884f9af4e34107640029e822598dffdc884707
sarphiv <32750711+sarphiv@users.noreply.github.com>
2022-06-09 17:27:51 +0200

    Fixed incorrect legend

--- report-graphs/marginal_conditional_coverage.py
+++ report-graphs/marginal_conditional_coverage.py
@@ -22,14 +22,16 @@ d[g == 1] = rng.binomial(1, c1, np.sum(g == 1))
 
 #%%
 # Plot marginally
+plt.rc("font", size= 14)
+
 plt.plot(x[d == 0] - g[d == 0], y[d == 0], ".", c="green")
 plt.plot(x[d == 1] - g[d == 1], y[d == 1], ".", c="red")
-plt.text(0, -0.7, "All patients", ha="center")
+plt.text(0, -0.7, "All patients", size=20, ha="center")
 
 
 
 plt.axis("off")
-plt.legend(["Healthy", "Sick"])
+plt.legend(["Correct", "Wrong"])
 plt.figure(figsize=(8, 6), dpi=100)
 plt.show()
 
@@ -38,11 +40,11 @@ plt.show()
 plt.plot(x[d == 0], y[d == 0], ".", c="green")
 plt.plot(x[d == 1], y[d == 1], ".", c="red")
 plt.plot([0.5, 0.5], [-1, 1], "--", c="black")
-plt.text(0, -0.8, "Group 1", ha="center")
-plt.text(1, -0.8, "Group 2", ha="center")
+plt.text(0, -0.8, "Group 1", size=20, ha="center")
+plt.text(1, -0.8, "Group 2", size=20, ha="center")
 
 plt.axis("off")
-plt.legend(["Healthy", "Sick"])
+plt.legend(["Correct", "Wrong"])
 plt.figure(figsize=(8, 6), dpi=100)
 plt.show()
 
